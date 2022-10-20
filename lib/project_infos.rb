8bbc9873f91a0d06a510082b4204b3e7d7d7b3b2
jerry914 <joyce91400@hotmail.com>
2022-10-20 17:58:13 +0800

    change data source format

--- lib/project_infos.rb
+++ lib/project_infos.rb
@@ -22,41 +22,40 @@ end
 unsplash_response = {}
 unsplash_results = {}
 
-mushroom_picture_url = unsplash_api_path('photos/WPXxp36tkHQ')
+def call_unsplash_topic(config, topic)
+  topic_wallpapers_url = unsplash_api_path("topics/#{topic}")
+  topic_wallpapers = call_unsplash_url(config, topic_wallpapers_url).parse
+
+  topic_results = {}
+  topic_results['title'] = topic_wallpapers['title']
+  # Wallpapers
+  topic_results['description'] = topic_wallpapers['description']
+  # From epic drone shots to inspiring moments in nature — submit your best desktop and mobile backgrounds.\r\n\r\n
+  topic_results['topic_url'] = topic_wallpapers['links']['html']
+  # https://unsplash.com/t/wallpapers
+  return topic_results
+end
+
+mushroom_picture_url = unsplash_api_path('photos/-PcrXRjoP-A')
 unsplash_response[mushroom_picture_url] = call_unsplash_url(config, mushroom_picture_url)
 mushroom_photo = unsplash_response[mushroom_picture_url].parse
 
-unsplash_results['width'] = mushroom_photo['width']
+photo_results = {}
+photo_results['width'] = mushroom_photo['width']
 # width= 4021
-unsplash_results['height'] = mushroom_photo['height']
+photo_results['height'] = mushroom_photo['height']
 # height= 2262
-unsplash_results['urls'] = mushroom_photo['urls']['raw']
+photo_results['urls'] = mushroom_photo['urls']['raw']
 # urls = https://images.unsplash.com/photo-1617925109341-2b99305cdee2?ixid=MnwzNzE2OTd8MHwxfGFsbHx8fHx8fHx8fDE2NjU3MzU5NjU&ixlib=rb-1.2.1
-unsplash_results['likes'] = mushroom_photo['likes']
-# not sure originals 439 maybe changed
-unsplash_results['name'] = mushroom_photo['user']['name']
-# Gabriel Dizzi
-unsplash_results['uesr_bio'] = mushroom_photo['user']['bio']
-# "Hello, my name is Gabriel Vinicius and I'm a Brazilian photographer. I'm seventeen years old and for more images of my work you can see it on my instagram @ogabrieldizzi"
-unsplash_results['uesr_photo'] = mushroom_photo['user']['profile_image']['large']
+photo_results['creator'] = {
+  name: mushroom_photo['user']['name'], # Gabriel Dizzi
+  bio: mushroom_photo['user']['bio'], # "Hello, my name is Gabriel Vinicius and I'm a Brazilian photographer. I'm seventeen years old and for more images of my work you can see it on my instagram @ogabrieldizzi"
+  photo: mushroom_photo['user']['profile_image']['large'] #https://images.unsplash.com/profile-1649848634149-2787772c4ae8?ixlib=rb-4.0.3&crop=faces&fit=crop&w=128&h=128
+}
+photo_results['topics'] = mushroom_photo['topic_submissions'].keys.map{|topic| call_unsplash_topic(config, topic)}
 
-unsplash_results['topics'] = mushroom_photo['topic_submissions'].keys.join(',')
 # topics wallpapers
-
-# https://api.unsplash.com/topics/wallpapers/?client_id=key
-
-topic_wallpapers_url = unsplash_api_path("topics/#{unsplash_results['topics']}")
-unsplash_response[topic_wallpapers_url] = call_unsplash_url(config, topic_wallpapers_url)
-topic_wallpapers = unsplash_response[topic_wallpapers_url].parse
-
-unsplash_results['title'] = topic_wallpapers['title']
-# Wallpapers
-unsplash_results['description'] = topic_wallpapers['description']
-# From epic drone shots to inspiring moments in nature — submit your best desktop and mobile backgrounds.\r\n\r\n
-unsplash_results['topic_url'] = topic_wallpapers['links']['html']
-# https://unsplash.com/t/wallpapers
-unsplash_results['owner_name'] = topic_wallpapers['owners'][0]['name']
-# Unsplash
+unsplash_results['view'] = photo_results
 
 unsplash_bad_url = unsplash_api_path('people/000')
 unsplash_response[unsplash_bad_url] = call_unsplash_url(config, unsplash_bad_url)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,12 +12,12 @@ require 'webmock'
 
 require_relative '../lib/unsplash_api'
 
-ID = 'WPXxp36tkHQ'
+ID = '-PcrXRjoP-A'
 key_path = File.expand_path('../config/secrets.yml', __dir__)
 CONFIG = YAML.safe_load(File.read(key_path))
 UNSPLAH_TOKEN = CONFIG['UNSPLASH_SECRETS_KEY']
 output_path = File.expand_path('../spec/fixtures/unsplash_results.yml', __dir__)
-CORRECT = YAML.safe_load(File.read(output_path))
+CORRECT = YAML.unsafe_load(File.read(output_path))
 
 CASSETTES_FOLDER = 'spec/fixtures/cassettes'
 CASSETTE_FILE = 'unsplash_api'
