2091038ca566f6e157aa57fe36e032415707c311
Hsuan <lala092325@gmail.com>
2022-10-27 17:46:07 +0800

    use topic to find photo

--- lib/project_info.rb
+++ lib/project_info.rb
@@ -5,7 +5,7 @@ require 'http'
 
 key_path = File.expand_path('../config/secrets.yml', __dir__)
 config = YAML.safe_load(File.read(key_path))
-puts config['UNSPLASH_SECRETS_KEY']
+# puts config['UNSPLASH_SECRETS_KEY']
 
 # 'https://api.unsplash.com/photos/WPXxp36tkHQ/?client_id=key'
 def unsplash_api_path(path)
@@ -22,21 +22,28 @@ end
 unsplash_response = {}
 unsplash_results = {}
 
-def call_unsplash_topic(config, topic)
-  topic_wallpapers_url = unsplash_api_path("topics/#{topic}")
+def call_unsplash_topic(config)
+  topic_wallpapers_url = unsplash_api_path('topics/?per_page=30')
   topic_wallpapers = call_unsplash_url(config, topic_wallpapers_url).parse
 
   topic_results = {}
-  topic_results['title'] = topic_wallpapers['title']
-  # Wallpapers
-  topic_results['description'] = topic_wallpapers['description']
-  # From epic drone shots to inspiring moments in nature — submit your best desktop and mobile backgrounds.\r\n\r\n
-  topic_results['topic_url'] = topic_wallpapers['links']['html']
-  # https://unsplash.com/t/wallpapers
+  for i in 0..23
+    per_topic_results = {}
+    per_topic_results['topic_id'] = topic_wallpapers[i]['id']
+    per_topic_results['title'] = topic_wallpapers[i]['title']
+    # Wallpapers
+    per_topic_results['description'] = topic_wallpapers[i]['description']
+    # From epic drone shots to inspiring moments in nature — submit your best desktop and mobile backgrounds.\r\n\r\n
+    per_topic_results['topic_url'] = topic_wallpapers[i]['links']['html']
+    # https://unsplash.com/t/wallpapers
+    topic_results[i+1] = per_topic_results
+  end
   topic_results
 end
 
-mushroom_picture_url = unsplash_api_path('photos/-PcrXRjoP-A')
+topic_results = call_unsplash_topic(config)
+
+mushroom_picture_url = unsplash_api_path("photos/random/?topics=#{topic_results[1]['topic_id']}&orientation=landscape")
 unsplash_response[mushroom_picture_url] = call_unsplash_url(config, mushroom_picture_url)
 mushroom_photo = unsplash_response[mushroom_picture_url].parse
 
@@ -53,14 +60,20 @@ photo_results['creator'] = {
   # I'm seventeen years old and for more images of my work you can see it on my instagram @ogabrieldizzi"
   photo: mushroom_photo['user']['profile_image']['large'] # https://images.unsplash.com/profile-1649848634149-2787772c4ae8?ixlib=rb-4.0.3&crop=faces&fit=crop&w=128&h=128
 }
-photo_results['topics'] = mushroom_photo['topic_submissions'].keys.map { |topic| call_unsplash_topic(config, topic) }
+photo_results['topics'] = mushroom_photo['topic_submissions']
+#photo_results['topics'] = mushroom_photo['topic_submissions'].keys.map { |topic| call_unsplash_topic(config, topic) }
 
 # topics wallpapers
 unsplash_results['view'] = photo_results
+unsplash_results['topics'] = topic_results
 
 unsplash_bad_url = unsplash_api_path('people/000')
 unsplash_response[unsplash_bad_url] = call_unsplash_url(config, unsplash_bad_url)
 unsplash_response[unsplash_bad_url].parse
 
+
 output_path = File.expand_path('../spec/fixtures/unsplash_results.yml', __dir__)
 File.write(output_path, unsplash_results.to_yaml)
+
+
+# puts call_unsplash_topic(config)
