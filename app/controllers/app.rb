8c879458747561e205fdbd9a249f9dc5acc202ac
jerry914 <joyce91400@hotmail.com>
2022-11-02 00:11:24 +0800

    add favorite page structure

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -26,6 +26,13 @@ module LightofDay
         view 'picktopic', locals: { topics: topics_data }
       end
 
+      routing.on 'favorite-list' do
+        routing.is do
+          favorite_list = [] # TODO: Jerry
+          view 'favoritelist', locals: { favoriteList: favorite_list }
+        end
+      end
+
       routing.on 'light-of-day' do
         routing.is do
           # POST /light-of-day/
@@ -33,18 +40,41 @@ module LightofDay
             topic_id = routing.params['topic_id']
             topic_data = topics_data.find { |topic| topic.topic_id == topic_id }
             routing.halt 404 unless topic_data
-            routing.redirect "light-of-day/#{topic_data.slug}"
+            routing.redirect "light-of-day/topic/#{topic_data.slug}"
           end
         end
 
-        routing.on String do |topic_slug|
-          # GET /light-of-day/{topic}
+        routing.on 'topic', String do |topic_slug|
+          # GET /light-of-day/topic/{topic}
           routing.get do
             topic_data = topics_data.find { |topic| topic.slug == topic_slug }
             routing.halt 404 unless topic_data
             view_data = LightofDay::Unsplash::ViewMapper.new(UNSPLAH_TOKEN, topic_data.topic_id).find_a_photo
             inspiration_data = LightofDay::FavQs::InspirationMapper.new.find_random
-            view 'view', locals: { view: view_data, inspiration: inspiration_data }
+            view 'view', locals: { view: view_data, inspiration: inspiration_data, is_saved: false }
+          end
+        end
+
+        routing.on 'favorite' do
+          routing.is do
+            # POST /light-of-day/favorite/
+            routing.post do
+              # view_id = routing.params['view_id']
+              view_id = 'fake_id'
+              puts 'store view & quote data' # TODO: Jerry
+              routing.halt 404 unless view_id
+              routing.redirect "favorite/#{view_id}"
+            end
+          end
+          routing.on String do |view_id|
+            # GET /light-of-day/favorite/{view_id}
+            routing.get do
+              # TODO: Jerry
+              view_data = LightofDay::Unsplash::ViewMapper.new(UNSPLAH_TOKEN, topics_data[0].topic_id).find_a_photo
+              inspiration_data = LightofDay::FavQs::InspirationMapper.new.find_random
+              # routing.halt 404 unless view_data && inspiration_data
+              view 'view', locals: { view: view_data, inspiration: inspiration_data, is_saved: true }
+            end
           end
         end
       end
