57da0b117e1c284732ebaac7447d3cda78ed45d6
Hsuan <lala092325@gmail.com>
2022-11-03 20:57:26 +0800

    aware of the various environments

--- config/environment.rb
+++ config/environment.rb
@@ -2,11 +2,25 @@
 
 require 'roda'
 require 'yaml'
+require 'figaro'
 
 module LightofDay
   # Configuration for the App
   class App < Roda
-    CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
-    UNSPLAH_TOKEN = CONFIG['UNSPLASH_SECRETS_KEY']
+    # CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
+    # UNSPLAH_TOKEN = CONFIG['UNSPLASH_SECRETS_KEY']
+    plugin :environments
+
+    # rubocop:disable Lint/ConstantDefinitionInBlock
+    configure do
+      # Environment variables setup
+      Figaro.application = Figaro::Application.new(
+        environment:,
+        path: File.expand_path('config/secrets.yml')
+      )
+      Figaro.load
+
+      def self.config = Figaro.env
+    end
   end
 end
