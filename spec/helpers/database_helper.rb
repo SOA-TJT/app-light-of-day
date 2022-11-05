1582012ac39ae616a23c72f5c0a551c639c32490
Hsuan <lala092325@gmail.com>
2022-11-05 11:48:40 +0800

    debug "destroy"

--- spec/helpers/database_helper.rb
+++ spec/helpers/database_helper.rb
@@ -5,8 +5,8 @@ module DatabaseHelper
   def self.wipe_database
     # Ignore foreign key constraints when wiping tables
     LightofDay::App.DB.run('PRAGMA foreign_keys =OFF')
-    LightofDay::Database::InspirationOrm.map(&:destory)
-    LightofDay::Database::ViewOrm.map(&:destory)
+    LightofDay::Database::InspirationOrm.map(&:destroy)
+    LightofDay::Database::ViewOrm.map(&:destroy)
     LightofDay::App.DB.run('PRAGMA foreign_keys =ON')
   end
 end
