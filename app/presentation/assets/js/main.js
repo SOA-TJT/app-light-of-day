d853e490b1a9027f987d74f9b62e26933151cec8
a0716116z <65119444+a0716116z@users.noreply.github.com>
2022-11-20 19:37:00 +0800

    update icons

--- app/presentation/assets/js/main.js
+++ app/presentation/assets/js/main.js
@@ -1,12 +1,4 @@
-// console.log('read the data');
 $(document).ready(function(){
-  // console.log('test has start');
-  // $(".alert").click(function(){
-  //     // console.log("button has clicked")
-  //     $(".alert").fadeTo(2000,500).slideUp(500, function() {
-  //     $(".alert").slideUp(500);
-  //   });
-  // });
   $(".alert").fadeTo(2000,500).slideUp(500, function() {
     $(".alert").slideUp(500);
   });
