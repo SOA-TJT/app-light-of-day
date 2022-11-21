0bfc8c3372133be8abafaeaeff11e4f826e685a6
a0716116z <65119444+a0716116z@users.noreply.github.com>
2022-11-21 09:01:19 +0800

    fix the js error

--- app/presentation/assets/js/main.js
+++ app/presentation/assets/js/main.js
@@ -2,4 +2,6 @@ $(document).ready(function(){
   $(".alert").fadeTo(2000,500).slideUp(500, function() {
     $(".alert").slideUp(500);
   });
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 });
