c1b48f2866d54d42b8b03306a278aa70c3062fd6
jerry914 <joyce91400@hotmail.com>
2022-12-18 21:54:50 +0800

    delete useless code & add secrets_example

--- app/application/services/find_topics.rb
+++ app/application/services/find_topics.rb
@@ -11,35 +11,12 @@ module LightofDay
         @topics_mapper = LightofDay::TopicMapper.new(App.config.UNSPLASH_SECRETS_KEY)
       end
 
-      # def call(type)
-      #   data = if type == 'normal'
-      #            @topics_mapper.topics
-      #          elsif type == 'created_time'
-      #            @topics_mapper.created_time
-      #          elsif type == 'activeness'
-      #            @topics_mapper.activeness
-      #          else
-      #            @topics_mapper.popularity
-      #          end
-      #   Success(data)
-      # rescue StandardError
-      #   Failure('Having trouble accessing the topics data')
-      # end
-
       def call(slug)
         chosed_topic_data = @topics_mapper.topics.find { |topic| topic.slug == slug }
         Success(chosed_topic_data)
       rescue StandardError
         Failure('Having trouble accessing the topics data')
       end
-
-      # def find_slug(topic_id)
-      #   chosed_topic_data = @topics_mapper.topics.find { |topic| topic.topic_id == topic_id }
-      #   current_slug = chosed_topic_data.slug
-      #   Success(current_slug)
-      # rescue StandardError
-      #   Failure('Having trouble accessing the slug')
-      # end
     end
   end
 end
